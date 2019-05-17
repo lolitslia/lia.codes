70a3b654851ae06231903db843728095031ed65e
liaadele <lastein@mac.com>
2019-05-16 22:26:29 -0400

    more images and interior pages

---
+++ scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    //minmax the fake browser
+    if($('.list-text').length){
+        $('.minmax').click(function() {
+            $('.list-text').toggleClass('open');
+            $('.minmax').toggleClass('open');
+        });
+    }
+
+    //show date time
+    function dayTimeFormat() {
+        var date = new Date;
+        var month = date.getMonth()+1;
+        var day = date.getDate();
+        var year = date.getFullYear();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = month + '/' + day + '/' + year + ' ' + hours + ':' + minutes + '' + ampm;
+        var vlTime = month + '-' + day + '-' + year;
+        console.log(strTime);
+        $('.time time').attr('datetime',vlTime);
+        $('.time time').text(strTime);
+        return strTime;
+    }
+    dayTimeFormat();
+    $(function () {
+        setInterval(dayTimeFormat, 1000);
+    });
+
+    //day and night modes
+    $('.fa-moon').click(function() {
+        $(this).addClass('active');
+        $('.fa-sun').removeClass('active');
+        $('body').addClass('night');
+        $('body').removeClass('day');
+    });
+    $('.fa-sun').click(function() {
+        $(this).addClass('active');
+        $('.fa-moon').removeClass('active');
+        $('body').addClass('day');
+        $('body').removeClass('night');
+    });
+
+});
